92e66f4e6c10e920022e8dae7c50afedf3db9744
ianrigsbee54 <irrigsbee@miners.utep.edu>
2020-09-27 22:17:56 -0600

    made coding style changes and added comments

--- src/tokenizermain.c
+++ src/tokenizermain.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include "tokenizer.h"
 
-
-
+/*checks if char c is a space, tab, or the null terminator*/
 int space_char(char c)
 {
   if(c == ' '){
@@ -17,7 +14,7 @@ int space_char(char c)
   }
   return 0;
 }
-
+/*checks if char c is a not a space and is a letter,digit,or symbol*/
 int non_space_char(char c)
 {
   if(c == '\0'){
@@ -28,74 +25,77 @@ int non_space_char(char c)
   }
   return 0;
 }
-
+/*iterates until we reach the start of a word*/
 char *word_start(char *str)
 {
   while(space_char(*str)){
     str++;
   }
-  return str;
+  return str;/*return a pointer pointing to the start of a word*/
 }
+/*iterates until we reach the first space character*/
 char *word_terminator(char *word)
 {
   while(non_space_char(*word)){
     word++;
   }
-  return word;
+  return word;/*return a pointer pointing to the start of a space*/
 }
-
+/*counts the number of words within a string and return that count*/
 int count_words(char *str)
 {
   int count = 0;
-  while(*str != '\0'){
+  while(*str != '\0'){/*iterate until we reach the null terminator*/
     str = word_start(str);
     str = word_terminator(str);
-    count++;
+    count++;/*once we reach the end of a word we increment count*/
   }
   return count;
 }
-
+/*copies the string up until len into a memory allocated string and returns it*/
 char *copy_str(char *inStr, short len)
 {
-  char *copied_str = (char*) malloc(sizeof(char)*(len + 1));
+  char *copied_str = (char*) malloc(sizeof(char)*(len + 1));/*allocated memory for len + 1*/
   int i = 0;
-  while(i < len){
+  while(i < len){/*start inserting into new string*/
     copied_str[i] = inStr[i];
     i++;
   }
-  copied_str[len] = '\0';
+  copied_str[len] = '\0';/*set the last part of the string to the null terminator*/
   return copied_str;
 }
 
 char **tokenize(char* str)
 {
-  int length = count_words(str);
+  int length = count_words(str);/*count number of words in string*/
   char **char_array;
-  char_array = (char**)malloc(sizeof(char*) * (length + 1));
+  char_array = (char**)malloc(sizeof(char*) * (length + 1));/*allocate memory for our vector*/
   int i = 0;
-  while(i < length-1){
+  
+  while(i < length - 1){/*start filling vector with pointers returned from copy_str*/
     str = word_start(str);
-    int sizediff = word_terminator(str) - str;
+    int sizediff = word_terminator(str) - str;/*find the difference in length*/
     char_array[i] = copy_str(str,sizediff);
-    str = word_terminator(str);
+    str = word_terminator(str);/*set str to start at the next space char for word_start*/
     i++;
   }
-  char_array[i] = NULL;
+  char_array[i] = NULL;/*set last part of char_array to NULL*/
   return char_array;
 }
 void print_tokens(char **tokens)
 {
   int i;
-  for(i = 0; tokens[i] != NULL; i++){
+  for(i = 0; tokens[i] != NULL; i++){/*iterate through and print the contents of tokens*/
     printf("%s\n", tokens[i]);
   }
 }
+
 void free_tokens(char **tokens)
 {
   int i = 0;
-  while(tokens[i] != NULL){
+  while(tokens[i] != NULL){/*iterate through and free the contents of tokens*/
     free(tokens[i]);
     i++;
   }
-  free(tokens);
+  free(tokens);/*last we free tokens itself as we allocated memory for it*/
 }
--- src/ui.c
+++ src/ui.c
@@ -3,47 +3,47 @@
 #include "tokenizer.h"
 #include "history.h"
 
-#define MAX 50
+#define MAX 50 /*max characters for input*/
 int main()
 {
-  char input[MAX];
-  char **token;
-  List *hist = init_history();
+  char input[MAX]; /*input array for user input*/
+  char **token; /*used for our tokenize method*/
+  List *hist = init_history(); /*initialize history*/
   printf("Welcome to tokenizer\n");
   printf("here are your commands\n");
   printf("q : quits program\n");
   printf("!x : brings up history at certain index x\n");
   
-  while(*input != 'q'){
+  while(*input != 'q' && input[1] != '\0'){/*makes sure the loop doesnt break because of q*/
     printf("$ ");
     fgets(input, MAX, stdin);
     if(*input != '!' && *input != 'q'){
       printf("string entered is %s\n", input);
-      token = tokenize(input);
+      token = tokenize(input); /*initialize our **token and fill the contents*/
       printf("tokenized string is\n");
       
-      print_tokens(token);
+      print_tokens(token); /*print our tokens*/
       printf("freeing tokens\n");
-      free_tokens(token);
+      free_tokens(token); /*free them*/
       printf("adding to history\n");
-      add_history(hist, input);
+      add_history(hist, input); /*then add our input to our history list*/
     }
-    if(*input == '!'){
-      int index = (int)(input[1]-'0');
-      char *histStr = get_history(hist, index);
+    if(*input == '!'){/*check if user inputed ! for history lookup*/
+      int index = (int)(input[1]); 
+      char *histStr = get_history(hist, index);/*get specific history at index*/
       printf("item at index %i%s\n",index, histStr);
     }
-    if(*input == 'q'){
+    if(*input == 'q'){/*break out of loop to finish user input*/
       break;
     }
   
     
   }
   printf("All of your history strings were\n");
-  print_history(hist);
+  print_history(hist); /*print user history*/
   printf("Now clearing history\n");
-  free_history(hist);
-  printf("Thank you for using tokenizer\n");
+  free_history(hist);/*frees our history list before exiting program*/
+  printf("Thank you for using tokenizer!\n");
   return 0;
   
 }
